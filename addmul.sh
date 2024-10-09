816a7b28d8285ee361dad1441ae4f5c174202f09
Ubuntu <ubuntu@ip-172-31-46-96.ap-south-1.compute.internal>
2024-10-09 06:53:01 +0000

    5th commit add.sh is changed with function addmul

--- addmul.sh
+++ addmul.sh
@@ -8,4 +8,7 @@ echo " Number c:"
 read c
 
 sum=$(($a + $b + $c))
-echo " Sum of $a ,$b  and $c is = : $sum"
+mul=$(($a * $b * $c))
+echo " The Sum of $a ,$b  and $c is = : $sum"
+echo " The multiplication of $a , $b and $c is = : $mul"
+
